c2354aa61e7e878e0b2a90ec260537844efca242
Domizio Demichelis <dd.nexus@gmail.com>
2018-02-17 18:19:33 +0700

    refactoring of *_nav methods: passed opts must not get added to the pagy object

--- lib/pagy/frontend.rb
+++ lib/pagy/frontend.rb
@@ -44,39 +44,43 @@ class Pagy
 
     # Generic pagination: it returns the html with the series of links to the pages
     def pagy_nav(pagy, opts=nil)
-      pagy.opts.merge!(opts) if opts ; link = pagy_link_proc(pagy) ; tags = []  # init all vars
+      opts = opts ? pagy.opts.merge(opts) : pagy.opts
+      link = pagy_link_proc(pagy)
+      tags = []
 
       tags << (pagy.prev ? %(<span class="page prev">#{link.call pagy.prev, pagy_t('pagy.nav.prev'.freeze), 'aria-label="previous"'.freeze}</span>)
                          : %(<span class="page prev disabled">#{pagy_t('pagy.nav.prev'.freeze)}</span>))
       pagy.series.each do |item|  # series example: [1, :gap, 7, 8, "9", 10, 11, :gap, 36]
         tags << case item
-                  when Integer; %(<span class="page">#{link.call item}</span>)                  # page link
+                  when Integer; %(<span class="page">#{link.call item}</span>)                    # page link
                   when String ; %(<span class="page active">#{item}</span>)                       # current page
                   when :gap   ; %(<span class="page gap">#{pagy_t('pagy.nav.gap'.freeze)}</span>) # page gap
                 end
       end
       tags << (pagy.next ? %(<span class="page next">#{link.call pagy.next, pagy_t('pagy.nav.next'.freeze), 'aria-label="next"'.freeze}</span>)
                          : %(<span class="page next disabled">#{pagy_t('pagy.nav.next'.freeze)}</span>))
-      %(<nav class="#{pagy.opts[:class]||'pagination'.freeze}" role="navigation" aria-label="pager">#{tags.join(pagy.opts[:separator]||' '.freeze)}</nav>)
+      %(<nav class="#{opts[:class]||'pagination'.freeze}" role="navigation" aria-label="pager">#{tags.join(opts[:separator]||' '.freeze)}</nav>)
     end
 
 
     # Pagination for bootstrap: it returns the html with the series of links to the pages
     def pagy_nav_bootstrap(pagy, opts=nil)
-      pagy.opts.merge!(opts) if opts ; link = pagy_link_proc(pagy, 'class="page-link"'.freeze) ; tags = []  # init all vars
+      opts = opts ? pagy.opts.merge(opts) : pagy.opts
+      link = pagy_link_proc(pagy, 'class="page-link"'.freeze)
+      tags = []
 
       tags << (pagy.prev ? %(<li class="page-item prev">#{link.call pagy.prev, pagy_t('pagy.nav.prev'.freeze), 'aria-label="previous"'.freeze}</li>)
                          : %(<li class="page-item prev disabled"><a href="#" class="page-link">#{pagy_t('pagy.nav.prev'.freeze)}</a></li>))
       pagy.series.each do |item| # series example: [1, :gap, 7, 8, "9", 10, 11, :gap, 36]
         tags << case item
-                  when Integer; %(<li class="page-item">#{link.call item}</li>)                           # page link
-                  when String ; %(<li class="page-item active">#{link.call item}</li>)                    # active page
+                  when Integer; %(<li class="page-item">#{link.call item}</li>)                                                               # page link
+                  when String ; %(<li class="page-item active">#{link.call item}</li>)                                                        # active page
                   when :gap   ; %(<li class="page-item gap disabled"><a href="#" class="page-link">#{pagy_t('pagy.nav.gap'.freeze)}</a></li>) # page gap
                 end
       end
       tags << (pagy.next ? %(<li class="page-item next">#{link.call pagy.next, pagy_t('pagy.nav.next'.freeze), 'aria-label="next"'.freeze}</li>)
                          : %(<li class="page-item next disabled"><a href="#" class="page-link">#{pagy_t('pagy.nav.next'.freeze)}</a></li>))
-      %(<nav class="#{pagy.opts[:class]||'pagination'.freeze}" role="navigation" aria-label="pager"><ul class="pagination">#{tags.join}</ul></nav>)
+      %(<nav class="#{opts[:class]||'pagination'.freeze}" role="navigation" aria-label="pager"><ul class="pagination">#{tags.join}</ul></nav>)
     end
 
 
