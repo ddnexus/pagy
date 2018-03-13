393d0073e72b96806ed9a9b0099d49fa7bd2855d
Domizio Demichelis <dd.nexus@gmail.com>
2018-03-13 13:11:21 +0700

    refactoring of Backend

--- lib/pagy/backend.rb
+++ lib/pagy/backend.rb
@@ -1,33 +1,24 @@
 # See Pagy::Backend API documentation: https://ddnexus.github.io/pagy/api/backend
 
 class Pagy
-
   module Backend ; private         # the whole module is private so no problem with including it in a controller
 
-    # returnthe pagy object and the scope of the page of records
-    def pagy(obj, vars={})
-      pagy = Pagy.new(count: pagy_get_count(obj), page: pagy_get_page, i18n_key: pagy_get_i18n_key(obj), **vars)
-      return pagy, pagy_get_items(obj, pagy)
+    # return pagy object and items
+    def pagy(collection, vars=nil)
+      pagy = Pagy.new(vars ? pagy_get_vars(collection).merge!(vars) : pagy_get_vars(collection))   # conditional merge is faster and saves memory
+      return pagy, pagy_get_items(collection, pagy)
     end
 
-    # get the collection count
-    def pagy_get_count(obj)
-      obj.count
+    # sub-method called only by #pagy: here for easy customization of variables by overriding
+    def pagy_get_vars(collection)
+      # return the variables to initialize the pagy object
+      { count: collection.count, page: params[:page] }
     end
 
-    # get the page number
-    def pagy_get_page
-      params[:page]
-    end
-
-    # get the i18n key for the collection
-    def pagy_get_i18n_key(obj) end
-
-    # get the page of records
-    # this should work with ActiveRecord, Sequel, Mongoid...
-    # override it if obj does not implement it that way
-    def pagy_get_items(obj, pagy)
-      obj.offset(pagy.offset).limit(pagy.items)
+    # sub-method called only by #pagy: here for easy customization of record-extraction by overriding
+    def pagy_get_items(collection, pagy)
+      # this should work with ActiveRecord, Sequel, Mongoid...
+      collection.offset(pagy.offset).limit(pagy.items)
     end
 
   end
