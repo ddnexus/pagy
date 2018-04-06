cee02c61f09f5eece2e9e83289bcec50731cab49
Domizio Demichelis <dd.nexus@gmail.com>
2018-04-06 23:25:34 +0700

    helper tags are strings

--- lib/pagy/frontend.rb
+++ lib/pagy/frontend.rb
@@ -10,25 +10,25 @@ class Pagy
 
     # Generic pagination: it returns the html with the series of links to the pages
     def pagy_nav(pagy)
-      tags = []; link = pagy_link_proc(pagy)
+      tags = ''; link = pagy_link_proc(pagy)
 
-      tags << (pagy.prev ? %(<span class="page prev">#{link.call pagy.prev, pagy_t('pagy.nav.prev'.freeze), 'aria-label="previous"'.freeze}</span>)
-                         : %(<span class="page prev disabled">#{pagy_t('pagy.nav.prev'.freeze)}</span>))
+      tags << (pagy.prev ? %(<span class="page prev">#{link.call pagy.prev, pagy_t('pagy.nav.prev'.freeze), 'aria-label="previous"'.freeze}</span> )
+                         : %(<span class="page prev disabled">#{pagy_t('pagy.nav.prev'.freeze)}</span> ))
       pagy.series.each do |item|  # series example: [1, :gap, 7, 8, "9", 10, 11, :gap, 36]
-        tags << if    item.is_a?(Integer); %(<span class="page">#{link.call item}</span>)                    # page link
-                elsif item.is_a?(String) ; %(<span class="page active">#{item}</span>)                       # current page
-                elsif item == :gap       ; %(<span class="page gap">#{pagy_t('pagy.nav.gap'.freeze)}</span>) # page gap
+        tags << if    item.is_a?(Integer); %(<span class="page">#{link.call item}</span> )                    # page link
+                elsif item.is_a?(String) ; %(<span class="page active">#{item}</span> )                       # current page
+                elsif item == :gap       ; %(<span class="page gap">#{pagy_t('pagy.nav.gap'.freeze)}</span> ) # page gap
                 end
       end
       tags << (pagy.next ? %(<span class="page next">#{link.call pagy.next, pagy_t('pagy.nav.next'.freeze), 'aria-label="next"'.freeze}</span>)
                          : %(<span class="page next disabled">#{pagy_t('pagy.nav.next'.freeze)}</span>))
-      %(<nav class="pagination" role="navigation" aria-label="pager">#{tags.join(' '.freeze)}</nav>)
+      %(<nav class="pagination" role="navigation" aria-label="pager">#{tags}</nav>)
     end
 
 
     # Pagination for bootstrap: it returns the html with the series of links to the pages
     def pagy_nav_bootstrap(pagy)
-      tags = []; link = pagy_link_proc(pagy, 'class="page-link"'.freeze)
+      tags = ''; link = pagy_link_proc(pagy, 'class="page-link"'.freeze)
 
       tags << (pagy.prev ? %(<li class="page-item prev">#{link.call pagy.prev, pagy_t('pagy.nav.prev'.freeze), 'aria-label="previous"'.freeze}</li>)
                          : %(<li class="page-item prev disabled"><a href="#" class="page-link">#{pagy_t('pagy.nav.prev'.freeze)}</a></li>))
@@ -40,7 +40,7 @@ class Pagy
       end
       tags << (pagy.next ? %(<li class="page-item next">#{link.call pagy.next, pagy_t('pagy.nav.next'.freeze), 'aria-label="next"'.freeze}</li>)
                          : %(<li class="page-item next disabled"><a href="#" class="page-link">#{pagy_t('pagy.nav.next'.freeze)}</a></li>))
-      %(<nav class="pagination" role="navigation" aria-label="pager"><ul class="pagination">#{tags.join}</ul></nav>)
+      %(<nav class="pagination" role="navigation" aria-label="pager"><ul class="pagination">#{tags}</ul></nav>)
     end
 
 
@@ -79,7 +79,7 @@ class Pagy
     # define #pagy_t depending on I18N[:gem] and I18n
     if I18N[:gem] || I18N[:gem].nil? && defined?(I18n)
       I18n.load_path << I18N[:file]
-      def pagy_t(*a); I18n.t(*a) end
+      def pagy_t(*a) I18n.t(*a) end
     else
       # load data from the first locale in the file
       I18N_DATA = YAML.load_file(I18N[:file]).first[1].freeze
