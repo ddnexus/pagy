d85791e6890ba16fb434b00d4d9890737ec3da27
Domizio Demichelis <dd.nexus@gmail.com>
2018-06-01 18:28:52 +0700

    Refactoring I18N to fix #39: - moved I18N from Pagy to Pagy::Frontend - I18N_DATA moved to Pagy::Frontend::I18N[:data] - I18N[:file] removed - added I18N.load_file method - updated initializer and doc

--- lib/pagy.rb
+++ lib/pagy.rb
@@ -12,9 +12,6 @@ class Pagy ; VERSION = '0.8.3'
   # default core vars
   VARS = { page:1, items:20, outset:0, size:[1,4,4,1], page_param: :page, params: {} }
 
-  # default I18n vars
-  zero_one = [:zero, :one] ; I18N = { file: Pagy.root.join('locales', 'pagy.yml').to_s, plurals: -> (c) {(zero_one[c] || :other).to_s.freeze} }
-
   attr_reader :count, :page, :items, :vars, :pages, :last, :offset, :from, :to, :prev, :next
 
   # merge and validate the options, do some simple aritmetic and set the instance variables
