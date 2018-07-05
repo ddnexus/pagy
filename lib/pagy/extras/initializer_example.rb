60ee1a0afd808fb1bbc8e63d260050b89a6237d0
Yasuaki Goto <yasu1003@gmail.com>
2018-07-05 22:31:50 +0900

    fix typo

--- lib/pagy/extras/initializer_example.rb
+++ lib/pagy/extras/initializer_example.rb
@@ -56,7 +56,7 @@
 # Pagy::Frontend::I18N Constant
 # See https://ddnexus.github.io/pagy/api/frontend#i18n
 # Pagy::Frontend::I18N.load(file:'path/to/dictionary.yml', language:'en')           # load a custom file
-# Pagy::Frontend::I18N[:plural] = -> (count) {(['zero', 'one'][count] || 'other')   # default
+# Pagy::Frontend::I18N[:plural] = -> (count) {(['zero', 'one'][count] || 'other')}   # default
 
 
 # Rails: extras assets path required by compact, items and responsive extras
