002dd582624b91fac73eb762001ecf78c721df9c
Domizio Demichelis <dd.nexus@gmail.com>
2018-07-13 09:13:12 +0200

    version 0.13.0

--- lib/pagy.rb
+++ lib/pagy.rb
@@ -3,7 +3,7 @@
 
 require 'pathname'
 
-class Pagy ; VERSION = '0.12.0'
+class Pagy ; VERSION = '0.13.0'
 
   class OutOfRangeError < StandardError; attr_reader :pagy; def initialize(pagy) @pagy = pagy end; end
 
