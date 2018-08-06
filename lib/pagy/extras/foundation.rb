0d918954681effaeb6e6ccbaed68379068042736
Dave Wieringa <davew@userwise.com>
2018-08-06 15:51:34 -0400

    Remove Foundation's `pagination-previous` and `pagination-next` classes
    These classes add Foundation's pagination arrows to the Next and Previous links.
    I'm removing the classes since they don't do anything else and pagy already
    supplies arrows via `pagy_t` and the I18n dictionary.

--- lib/pagy/extras/foundation.rb
+++ lib/pagy/extras/foundation.rb
@@ -9,16 +9,16 @@ class Pagy
     def pagy_nav_foundation(pagy)
       html, link, p_prev, p_next = +'', pagy_link_proc(pagy), pagy.prev, pagy.next
 
-      html << (p_prev ? %(<li class="pagination-previous">#{link.call p_prev, pagy_t('pagy.nav.prev'), 'aria-label="previous"'}</li>)
-                      : %(<li class="pagination-previous disabled">#{pagy_t('pagy.nav.prev')}</li>))
+      html << (p_prev ? %(<li>#{link.call p_prev, pagy_t('pagy.nav.prev'), 'aria-label="previous"'}</li>)
+                      : %(<li class="disabled">#{pagy_t('pagy.nav.prev')}</li>))
       pagy.series.each do |item| # series example: [1, :gap, 7, 8, "9", 10, 11, :gap, 36]
         html << if    item.is_a?(Integer); %(<li>#{link.call item}</li>)                                                                # page link
                 elsif item.is_a?(String) ; %(<li class="current"><span class="show-for-sr">You're on page</span>#{link.call item}</li>) # active page
                 elsif item == :gap       ; %(<li class="ellipsis" aria-hidden="true"></li>)                                             # page gap
                 end
       end
-      html << (p_next ? %(<li class="pagination-next">#{link.call p_next, pagy_t('pagy.nav.next'), 'aria-label="next"'}</li>)
-                      : %(<li class="pagination-next disabled">#{pagy_t('pagy.nav.next')}</li>))
+      html << (p_next ? %(<li>#{link.call p_next, pagy_t('pagy.nav.next'), 'aria-label="next"'}</li>)
+                      : %(<li class="disabled">#{pagy_t('pagy.nav.next')}</li>))
       %(<nav class="pagy-nav-foundation" aria-label="Pagination"><ul class="pagination">#{html}</ul></nav>)
     end
 
