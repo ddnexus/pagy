1b8a080ccb158f56eda1a25f2c4d5dd6ca1b451c
Domizio Demichelis <dd.nexus@gmail.com>
2018-11-20 12:12:16 +0700

    a few fixes for the countless tests

--- test/pagy/countless_test.rb
+++ test/pagy/countless_test.rb
@@ -16,13 +16,12 @@ describe Pagy::Countless do
 
     let(:last_page) { 3 }
 
-    it 'jump to 2 page with empty collection' do
+    it 'raises exception with no retrieved items' do
       proc { Pagy::Countless.new(page: 2).finalize(0) }.must_raise Pagy::OverflowError
     end
 
-    it 'first page on empty collection' do
+    it 'initializes empty collection' do
       pagy, _ = backend.send(:pagy_countless, @empty_collection, page: 1)
-
       pagy.items.must_equal 20
       pagy.pages.must_equal 1
       pagy.last.must_equal 1
@@ -32,10 +31,9 @@ describe Pagy::Countless do
       pagy.next.must_be_nil
     end
 
-    it 'first page' do
+    it 'initializes first page' do
       pagy, _ = backend.send(:pagy_countless, @collection, page: 1)
       pagy.must_be_instance_of Pagy::Countless
-
       pagy.items.must_equal 20
       pagy.last.must_equal 2
       pagy.pages.must_equal 2 # current + 1. `Countless` does not know real count
@@ -45,9 +43,8 @@ describe Pagy::Countless do
       pagy.next.must_equal 2
     end
 
-    it 'when only one full page exists' do
+    it 'initializes single full page' do
       pagy, _ = backend.send(:pagy_countless, TestCollection.new(Array(1..20)), page: 1)
-
       pagy.items.must_equal 20
       pagy.pages.must_equal 1
       pagy.from.must_equal 1
@@ -55,10 +52,9 @@ describe Pagy::Countless do
       pagy.prev.must_be_nil
       pagy.next.must_be_nil
     end
-    
-    it 'when only one not full page exists' do
-      pagy, _ = backend.send(:pagy_countless, TestCollection.new(Array(1..4)), page: 1)
 
+    it 'initialize single partial page' do
+      pagy, _ = backend.send(:pagy_countless, TestCollection.new(Array(1..4)), page: 1)
       pagy.items.must_equal 4
       pagy.pages.must_equal 1
       pagy.from.must_equal 1
@@ -68,9 +64,8 @@ describe Pagy::Countless do
 
     end
 
-    it 'when last page has less records then var[:items]' do
+    it 'initializes last partial page' do
       pagy, _ = backend.send(:pagy_countless, @collection, page: last_page)
-
       pagy.items.must_equal 19
       pagy.pages.must_equal last_page
       pagy.from.must_equal 41
@@ -78,7 +73,7 @@ describe Pagy::Countless do
       pagy.prev.must_equal(last_page - 1)
       pagy.next.must_be_nil
     end
-    
+
   end
 
 end
--- test/pagy/extras/countless_test.rb
+++ test/pagy/extras/countless_test.rb
@@ -10,52 +10,52 @@ describe Pagy::Backend do
   let(:last_page) { 1000 / 20 }
 
   before do
-    @default_page_param = Pagy::Countless::VARS[:page_param]
+    @default_page_param = Pagy::VARS[:page_param]
     @collection = TestCollection.new((1..1000).to_a)
   end
 
   after do
-    Pagy::Countless::VARS[:page_param] = @default_page_param
+    Pagy::VARS[:page_param] = @default_page_param
   end
 
   describe "#pagy_countless" do
 
-    it 'vars[:size] = [1, 4, 4, 1], on first page only show Next page' do
+    it 'shows current and next for first page' do
       pagy, _ = backend.send(:pagy_countless, @collection, { size: [1, 4, 4, 1], page: 1 })
       pagy.series.must_equal ['1', 2]
       pagy.prev.must_be_nil
       pagy.next.must_equal 2
     end
 
-    it 'vars[:size] = [1, 4, 4, 1] on 25 page, show one next page and previous pages with :gap' do
+    it 'shows start-pages, :gap, before-pages, current and next for intermediate page' do
       pagy, _ = backend.send(:pagy_countless, @collection, {page: 25})
       pagy.series.must_equal [1, :gap, 21, 22, 23, 24, '25', 26]
       pagy.prev.must_equal 24
       pagy.next.must_equal 26
     end
 
-    it 'vars[:size] = [1, 4, 4, 1] on last page show previous pages with :gap' do
+    it 'shows start-pages, :gap, before-pages, current and next for last page' do
       pagy, _ = backend.send(:pagy_countless, @collection, {page: last_page})
       pagy.series.must_equal [1, :gap, 46, 47, 48, 49, '50']
       pagy.prev.must_equal 49
       pagy.next.must_be_nil
     end
 
-    it 'vars[:size] = [], on first page, should hide seris' do
+    it 'returns empty series for empty :size variable for first page' do
       pagy, _ = backend.send(:pagy_countless, @collection, {size: [], page: 1})
       pagy.series.must_equal []
       pagy.prev.must_be_nil
       pagy.next.must_equal 2
     end
 
-    it 'vars[:size] = [], in the middle, should hide seris' do
+    it 'returns empty series for empty :size variable for intermediate page' do
       pagy, _ = backend.send(:pagy_countless, @collection, {size: [], page: 25})
       pagy.series.must_equal []
       pagy.prev.must_equal 24
       pagy.next.must_equal 26
     end
 
-    it 'vars[:size] = [], on last page, should hide seris' do
+    it 'returns empty series for empty :size variable for last page' do
       pagy, _ = backend.send(:pagy_countless, @collection, {size: [], page: last_page})
       pagy.series.must_equal []
       pagy.prev.must_equal 49
@@ -63,30 +63,30 @@ describe Pagy::Backend do
     end
   end
 
-  describe 'page_param in privete method "pagy_countless_get_vars"' do
+  describe '#pagy_countless_get_vars' do
 
     let(:backend) { TestController.new({a: 'a', page: 3, page_number: 4}) }
-    
-    it 'page_param from defaults' do
-      Pagy::Countless::VARS[:page_param] = :page_number
+
+    it 'sets :page_param from defaults' do
+      Pagy::VARS[:page_param] = :page_number
       pagy, items = backend.send(:pagy_countless, @collection)
       pagy.page.must_equal 4
       items.must_equal Array(61..80)
     end
 
-    it 'page_param from vars' do
-      Pagy::Countless::VARS[:page_param] = :page
+    it 'sets :page_param from vars' do
+      Pagy::VARS[:page_param] = :page
       pagy, items = backend.send(:pagy_countless, @collection, {page_param: :page_number})
       pagy.page.must_equal 4
       items.must_equal Array(61..80)
     end
 
-    it 'bypass page_param with vars[:page]' do
-      Pagy::Countless::VARS[:page_param] = :page_number
+    it 'bypasses :page_param with :page variable' do
+      Pagy::VARS[:page_param] = :another_page_number
       pagy, items = backend.send(:pagy_countless, @collection, {page_param: :page_number, page: 1})
       pagy.page.must_equal 1
       items.must_equal Array(1..20)
     end
-    
+
   end
 end
