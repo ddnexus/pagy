25b5693e7fbc69965e378b300a08e3dbd8c1a073
Uysim <uysimty@gmail.com>
2019-03-07 10:35:15 +0700

    uuid cursor paginate

--- lib/pagy/extras/cursor.rb
+++ lib/pagy/extras/cursor.rb
@@ -33,7 +33,7 @@ class Pagy
     # Return Pagy object and items
     def pagy_cursor(collection, vars={}, options={})
       raise('ActiveRecord is not defined') unless defined?(ActiveRecord)
-      pagy = Pagy.new(pagy_array_get_vars(collection, vars))
+      pagy = Pagy.new(pagy_cursor_get_vars(collection, vars))
 
       items =  pagy_cursor_get_items(collection, pagy, pagy.position)
       pagy.has_more =  pagy_cursor_has_more?(items, pagy)
@@ -41,15 +41,19 @@ class Pagy
       return pagy, items
     end
 
-    def pagy_array_get_vars(collection, vars)
+    def pagy_cursor_get_vars(collection, vars)
       vars[:arel_table] = collection.arel_table
       vars[:primary_key] = collection.primary_key
       vars
     end
 
-    def pagy_cursor_get_items(collection, pagy, position)
-      sql_comparation = pagy.arel_table[pagy.primary_key].send(pagy.comparation, position)
-      collection.where(sql_comparation).reorder(pagy.order).limit(pagy.items)
+    def pagy_cursor_get_items(collection, pagy, position=nil)
+      if position.present?
+        sql_comparation = pagy.arel_table[pagy.primary_key].send(pagy.comparation, position)
+        collection.where(sql_comparation).reorder(pagy.order).limit(pagy.items)
+      else
+        collection.reorder(pagy.order).limit(pagy.items)
+      end
     end
 
     def pagy_cursor_has_more?(collection, pagy)
--- lib/pagy/extras/uuid_cursor.rb
+++ lib/pagy/extras/uuid_cursor.rb
@@ -1,6 +1,10 @@
+
+
 class Pagy
 
-  attr_reader :before, :after, :arel_table, :primary_key, :order
+  attr_reader :before, :after, :arel_table, :primary_key, :order, :comparation, :position
+  attr_accessor :has_more
+  alias_method :has_more?, :has_more
 
   def initialize(vars)
     @vars = VARS.merge(vars.delete_if{|_,v| v.nil? || v == '' })
@@ -14,43 +18,54 @@ class Pagy
       raise(ArgumentError, 'before and after can not be both mentioned')
     end
 
+    if @before.present?
+      @comparation = 'lt'
+      @position = @before
+      @order = { :created_at => :desc , @primary_key => :desc }
+    end
 
-    @order = { @primary_key => :desc } if @before.present?
-    @order = { @primary_key => :asc } if @after.present?
+    if @after.present?
+      @comparation = 'gt'
+      @position = @after
+      @order = { :created_at => :desc , @primary_key => :asc }
+    end
   end
 
   module Backend ; private         # the whole module is private so no problem with including it in a controller
 
     # Return Pagy object and items
-    def pagy_cursor(collection, vars={}, options={})
+    def pagy_uuid_cursor(collection, vars={}, options={})
       raise('ActiveRecord is not defined') unless defined?(ActiveRecord)
-      pagy = Pagy.new(pagy_array_get_vars(collection, vars))
-      return pagy, pagy_cursor_get_items(collection, pagy)
+      pagy = Pagy.new(pagy_uuid_cursor_get_vars(collection, vars))
+      items =  pagy_uuid_cursor_get_items(collection, pagy, pagy.position)
+      pagy.has_more =  pagy_uuid_cursor_has_more?(items, pagy)
+
+      return pagy, items
     end
 
-    def pagy_array_get_vars(collection, vars)
+    def pagy_uuid_cursor_get_vars(collection, vars)
       vars[:arel_table] = collection.arel_table
       vars[:primary_key] = collection.primary_key
       vars
     end
 
-    # Sub-method called only by #pagy: here for easy customization of record-extraction by overriding
-    def pagy_cursor_get_items(collection, pagy)
-      # This should work with ActiveRecord
-      records = collection
-      records = pagy_cursor_before(records, pagy)  if pagy.before.present?
-      records = pagy_cursor_after(records, pagy) if pagy.after.present?
-      records.limit(pagy.items)
+    def pagy_uuid_cursor_get_items(collection, pagy, position=nil)
+      if position.present?
+        arel_table = pagy.arel_table
+
+        select_created_at = arel_table.project(arel_table[:created_at]).where(arel_table[pagy.primary_key].eq(position))
+        select_the_sample_created_at = arel_table[:created_at].eq(select_created_at).and(arel_table[pagy.primary_key].gt(position))
+        sql_comparation = arel_table[:created_at].gt(select_created_at).or(select_the_sample_created_at)
+
+        collection.where(sql_comparation).reorder(pagy.order).limit(pagy.items)
+      else
+        collection.reorder(pagy.order).limit(pagy.items)
+      end
     end
 
-    def pagy_cursor_before(collection, pagy)
-      # This should work with ActiveRecord
-      collection.where(pagy.arel_table[pagy.primary_key].lt(pagy.before)).reorder(pagy.order)
-    end
-
-    def pagy_cursor_after(collection, pagy)
-      # This should work with ActiveRecord
-      collection.where(pagy.arel_table[pagy.primary_key].gt(pagy.after)).reorder(pagy.order)
+    def pagy_uuid_cursor_has_more?(collection, pagy)
+      next_position = collection.last[pagy.primary_key]
+      pagy_uuid_cursor_get_items(collection, pagy, next_position).exists?
     end
   end
 end
