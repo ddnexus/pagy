7f928177266e194c1ed7cca0fbe1a77283cdc761
Domizio Demichelis <dd.nexus@gmail.com>
2019-03-13 21:17:03 +0700

    version 2.1.1

--- lib/pagy.rb
+++ lib/pagy.rb
@@ -4,7 +4,7 @@
 
 require 'pathname'
 
-class Pagy ; VERSION = '2.1.0'
+class Pagy ; VERSION = '2.1.1'
 
   class OverflowError < StandardError; attr_reader :pagy; def initialize(pagy) @pagy = pagy end; end
 
