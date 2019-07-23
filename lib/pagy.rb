05bc3d4030a07e4c1c51600886dbe57fe3ca7df2
Domizio Demichelis <dd.nexus@gmail.com>
2019-07-23 09:45:00 +0200

    version 3.4.1

--- lib/pagy.rb
+++ lib/pagy.rb
@@ -4,7 +4,7 @@
 
 require 'pathname'
 
-class Pagy ; VERSION = '3.4.0'
+class Pagy ; VERSION = '3.4.1'
 
   # Root pathname to get the path of Pagy files like templates or dictionaries
   def self.root; @root ||= Pathname.new(__FILE__).dirname.freeze end
