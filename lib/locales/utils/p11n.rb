d5f8e9445d72781347ac2306b0005a8ae8e328f7
nedimdz <nedim@wizardhealth.co>
2021-03-11 09:10:36 +0100

    Add translate for Bosnian and Croatian language

--- lib/locales/utils/p11n.rb
+++ lib/locales/utils/p11n.rb
@@ -67,9 +67,11 @@ p11n = {
 # The default pluralization for locales not explicitly listed here
 # is the :one_other pluralization proc (used for English)
 plurals = Hash.new(p11n[:one_other]).tap do |hash|
+  hash['bs']    = p11n[:east_slavic]
   hash['cs']    = p11n[:west_slavic]
   hash['id']    = p11n[:other]
   hash['fr']    = p11n[:one_upto_two_other]
+  hash['hr']    = p11n[:east_slavic]
   hash['ja']    = p11n[:other]
   hash['km']    = p11n[:other]
   hash['ko']    = p11n[:other]
