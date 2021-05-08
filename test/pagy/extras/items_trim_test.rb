94e262ff299bfd6c5ee17347d7fc5a56b5835677
Domizio Demichelis <dd.nexus@gmail.com>
2021-05-08 17:17:01 +0700

    added label tag to the pagy inputs

--- test/pagy/extras/items_trim_test.rb
+++ test/pagy/extras/items_trim_test.rb
@@ -26,12 +26,12 @@ describe 'pagy/extras/items_trim' do
   it 'renders items selector with trim' do
     pagy = Pagy.new count: 1000, page: 3
     _(view.pagy_items_selector_js(pagy, pagy_id: 'test-id')).must_equal \
-      "<span id=\"test-id\" class=\"pagy-items-selector-js\">Show <input type=\"number\" min=\"1\" max=\"100\" value=\"20\" style=\"padding: 0; text-align: center; width: 3rem;\"> items per page</span><script type=\"application/json\" class=\"pagy-json\">[\"items_selector\",41,\"<a href=\\\"/foo?page=__pagy_page__&items=__pagy_items__\\\"   style=\\\"display: none;\\\"></a>\",\"page\"]</script>"
+      "<span id=\"test-id\" class=\"pagy-items-selector-js\"><label>Show <input type=\"number\" min=\"1\" max=\"100\" value=\"20\" style=\"padding: 0; text-align: center; width: 3rem;\"> items per page</label></span><script type=\"application/json\" class=\"pagy-json\">[\"items_selector\",41,\"<a href=\\\"/foo?page=__pagy_page__&items=__pagy_items__\\\"   style=\\\"display: none;\\\"></a>\",\"page\"]</script>"
     _(view.pagy_items_selector_js(pagy, pagy_id: 'test-id', item_name: 'products')).must_equal \
-      "<span id=\"test-id\" class=\"pagy-items-selector-js\">Show <input type=\"number\" min=\"1\" max=\"100\" value=\"20\" style=\"padding: 0; text-align: center; width: 3rem;\"> products per page</span><script type=\"application/json\" class=\"pagy-json\">[\"items_selector\",41,\"<a href=\\\"/foo?page=__pagy_page__&items=__pagy_items__\\\"   style=\\\"display: none;\\\"></a>\",\"page\"]</script>"
+      "<span id=\"test-id\" class=\"pagy-items-selector-js\"><label>Show <input type=\"number\" min=\"1\" max=\"100\" value=\"20\" style=\"padding: 0; text-align: center; width: 3rem;\"> products per page</label></span><script type=\"application/json\" class=\"pagy-json\">[\"items_selector\",41,\"<a href=\\\"/foo?page=__pagy_page__&items=__pagy_items__\\\"   style=\\\"display: none;\\\"></a>\",\"page\"]</script>"
     Pagy::I18n['en'][0]['activerecord.models.product.other'] = ->(_){ 'products'}
     _(view.pagy_items_selector_js(pagy, pagy_id: 'test-id', i18n_key: 'activerecord.models.product')).must_equal \
-      "<span id=\"test-id\" class=\"pagy-items-selector-js\">Show <input type=\"number\" min=\"1\" max=\"100\" value=\"20\" style=\"padding: 0; text-align: center; width: 3rem;\"> products per page</span><script type=\"application/json\" class=\"pagy-json\">[\"items_selector\",41,\"<a href=\\\"/foo?page=__pagy_page__&items=__pagy_items__\\\"   style=\\\"display: none;\\\"></a>\",\"page\"]</script>"
+      "<span id=\"test-id\" class=\"pagy-items-selector-js\"><label>Show <input type=\"number\" min=\"1\" max=\"100\" value=\"20\" style=\"padding: 0; text-align: center; width: 3rem;\"> products per page</label></span><script type=\"application/json\" class=\"pagy-json\">[\"items_selector\",41,\"<a href=\\\"/foo?page=__pagy_page__&items=__pagy_items__\\\"   style=\\\"display: none;\\\"></a>\",\"page\"]</script>"
     pagy = Pagy.new count: 1000, page: 3, enable_items_extra: false
     _(view.pagy_items_selector_js(pagy, pagy_id: 'test-id')).must_equal ''
   end
