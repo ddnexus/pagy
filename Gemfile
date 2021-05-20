675f587b8f9b52933fd1dd491c5c2a0ff1de962e
Domizio Demichelis <dd.nexus@gmail.com>
2021-05-20 19:34:37 +0700

    Gemfile: added gemspec and updated rematch

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 
 source 'https://rubygems.org'
 
-# gemspec
+gemspec
 
 gem 'i18n'
 gem 'oj', require: false     # false is for testing with or without it
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,16 +3,13 @@
 $VERBOSE = {'false' => false, 'true' => true}[ENV['VERBOSE']] if ENV['VERBOSE']
 
 if ENV['CODECOV']
-  require 'codecov'   # requires also simplecov
+  require 'codecov'   # require also simplecov
   # if you want the formatter to upload the results use SimpleCov::Formatter::Codecov instead
   SimpleCov.formatter = Codecov::SimpleCov::Formatter  # upload with step in github actions
 elsif !ENV['CI']
   require 'simplecov'
 end
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-
-require 'pagy'
 require 'pagy/countless'
 require 'rack'
 require_relative 'mock_helpers/view'
