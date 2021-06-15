c73fb4c7c1c7a8288b97aca7f3629d551bbadb9c
Molfar <terryprogetto@gmail.com>
2021-06-15 19:51:02 +0300

    Meilisearch tests pass

--- lib/pagy/extras/meilisearch.rb
+++ lib/pagy/extras/meilisearch.rb
@@ -8,9 +8,41 @@ class Pagy
 
     # returns an array used to delay the call of #search
     # after the pagination variables are merged to the options
-    def pagy_searchkick(q = nil, **params)
-      [self, q, params]
+    def pagy_searchkick(term = nil, **vars)
+      [self, term, vars]
     end
     alias_method VARS[:meilisearch_search_method], :pagy_searchkick
+  end
+
+  # Add specialized backend methods to paginate Meilisearch results
+  module Backend
+    private
+
+    # Return Pagy object and results
+    def pagy_meilisearch(pagy_search_args, vars = {})
+      model, term, options = pagy_search_args
+      vars = pagy_meilisearch_get_vars(nil, vars)
+      options[:limit] = vars[:items]
+      options[:offset] = (vars[:page] - 1) * vars[:items]
+      results = model.search(term, **options)
+      vars[:count] = results.raw_answer[:nbHits]
+
+      pagy = Pagy.new(vars)
+
+      
+      return pagy_meilisearch(pagy_search_args, vars.merge(page: pagy.page)) \
+        if defined?(Pagy::UseOverflowExtra) && pagy.overflow? && pagy.vars[:overflow] == :last_page
+      
+      [pagy, results]
+    end
+
+    # Sub-method called only by #pagy_meilisearch: here for easy customization of variables by overriding
+    # the _collection argument is not available when the method is called
+    def pagy_meilisearch_get_vars(_collection, vars)
+      pagy_set_items_from_params(vars) if defined?(UseItemsExtra)
+      vars[:items] ||= VARS[:items]
+      vars[:page]  ||= (params[ vars[:page_param] || VARS[:page_param] ] || 1).to_i
+      vars
+    end
   end
 end
--- test/pagy/extras/meilisearch_test.rb
+++ test/pagy/extras/meilisearch_test.rb
@@ -20,4 +20,69 @@ describe 'pagy/extras/meilisearch' do
       _(MockMeilisearch::Model.pagy_search('a')).must_equal [MockMeilisearch::Model, 'a', {}]
     end
   end
+
+  describe 'controller_methods' do
+    let(:controller) { MockController.new }
+
+    describe '#pagy_meilisearch' do
+
+      before do
+        @collection = MockCollection.new
+      end
+
+      it 'paginates response with defaults' do
+        pagy, results = controller.send(:pagy_meilisearch, MockMeilisearch::Model.pagy_search('a'))
+        _(pagy).must_be_instance_of Pagy
+        _(pagy.count).must_equal 1000
+        _(pagy.items).must_equal Pagy::VARS[:items]
+        _(pagy.page).must_equal controller.params[:page]
+        _(results.length).must_equal Pagy::VARS[:items]
+        _(results).must_rematch
+      end
+
+      it 'paginates with vars' do
+        pagy, results = controller.send(:pagy_meilisearch, MockMeilisearch::Model.pagy_search('b'), page: 2, items: 10, link_extra: 'X')
+        _(pagy).must_be_instance_of Pagy
+        _(pagy.count).must_equal 1000
+        _(pagy.items).must_equal 10
+        _(pagy.page).must_equal 2
+        _(pagy.vars[:link_extra]).must_equal 'X'
+        _(results.length).must_equal 10
+        _(results).must_rematch
+      end
+
+      it 'paginates with overflow' do
+        pagy, results = controller.send(:pagy_meilisearch, MockMeilisearch::Model.pagy_search('b'), page: 200, items: 10, link_extra: 'X', overflow: :last_page)
+        _(pagy).must_be_instance_of Pagy
+        _(pagy.count).must_equal 1000
+        _(pagy.items).must_equal 10
+        _(pagy.page).must_equal 100
+        _(pagy.vars[:link_extra]).must_equal 'X'
+        _(results.length).must_equal 10
+        _(results).must_rematch
+      end
+    end
+
+    describe '#pagy_meilisearch_get_vars' do
+      it 'gets defaults' do
+        vars   = {}
+        merged = controller.send :pagy_meilisearch_get_vars, nil, vars
+        _(merged.keys).must_include :page
+        _(merged.keys).must_include :items
+        _(merged[:page]).must_equal 3
+        _(merged[:items]).must_equal 20
+      end
+
+      it 'gets vars' do
+        vars   = {page: 2, items: 10, link_extra: 'X'}
+        merged = controller.send :pagy_meilisearch_get_vars, nil, vars
+        _(merged.keys).must_include :page
+        _(merged.keys).must_include :items
+        _(merged.keys).must_include :link_extra
+        _(merged[:page]).must_equal 2
+        _(merged[:items]).must_equal 10
+        _(merged[:link_extra]).must_equal 'X'
+      end
+    end
+  end
 end
