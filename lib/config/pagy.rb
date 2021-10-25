e3e055af88b9d20e2a3c3187b8dc481e6ce26f75
Domizio Demichelis <dd.nexus@gmail.com>
2021-10-25 07:24:32 +0700

    Version 5.1.0

--- lib/config/pagy.rb
+++ lib/config/pagy.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Pagy initializer file (5.0.1)
+# Pagy initializer file (5.1.0)
 # Customize only what you really need and notice that Pagy works also without any of the following lines.
 # Should you just cherry pick part of this file, please maintain the require-order of the extras
 
