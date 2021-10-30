04c8804c4ea0c633c63e2c454e3d8f08345a04ad
Domizio Demichelis <dd.nexus@gmail.com>
2021-10-30 08:17:18 +0700

    Updated npm modules and gemfiles

--- test/e2e/ci-cache.sh
+++ test/e2e/ci-cache.sh
@@ -4,3 +4,6 @@ rm -rf ci-cache.lock
 for pkg in cypress @cypress/snapshot html-validate cypress-html-validate cypress-dark; do
   echo $pkg:"$(npm view $pkg version)" >> ci-cache.lock
 done
+echo 'The "ci-cache.lock" file has been generated with the following content:'
+echo
+cat ci-cache.lock
