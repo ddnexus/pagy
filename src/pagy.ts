e0ff0ffd9ce6ba83548effcb5d432a5d4b028956
Domizio Demichelis <dd.nexus@gmail.com>
2022-01-03 13:02:39 +0700

    Simpler pagy.ts

--- src/pagy.ts
+++ src/pagy.ts
@@ -36,31 +36,29 @@ const Pagy = {
         const elements = target.querySelectorAll("[data-pagy-json]");
         for (const element of elements) {
             const json = element.getAttribute("data-pagy-json");
-            if (json === null) {
-                continue;
-            }
+            if (json === null) { continue }
             const args = JSON.parse(json);
             const fname = args.shift() as "nav"|"combo_nav"|"items_selector";
             if (fname === "nav") {
                 Pagy.nav(element, ...args as [NavTags, NavSequels, null|NavLabelSequels, string]);
             } else if (fname === "combo_nav") {
-                Pagy.combo_nav(element, ...args as [string, string, string]);
+                Pagy.comboNav(element, ...args as [string, string, string]);
             } else if (fname === "items_selector") {
-                Pagy.items_selector(element, ...args as [number, string, string]);
+                Pagy.itemsSelector(element, ...args as [number, string, string]);
             }
         }
     },
 
     // Power the *_nav_js helpers
-    nav(pagyEl:Element, tags:NavTags, sequels:NavSequels, opt_label_sequels:null|NavLabelSequels, trimParam?:string) {
-        let label_sequels = {} as NavLabelSequels;
-        // Handle null label_sequels
-        if (opt_label_sequels === null) {
+    nav(pagyEl:Element, tags:NavTags, sequels:NavSequels, optLabelSequels:null|NavLabelSequels, trimParam?:string) {
+        let labelSequels = {} as NavLabelSequels;
+        // Handle null labelSequels
+        if (optLabelSequels === null) {
             for (const width in sequels) {
-                label_sequels[width] = sequels[width].map((item) => item.toString());
+                labelSequels[width] = sequels[width].map((item) => item.toString());
             }
         } else {
-            label_sequels = opt_label_sequels;
+            labelSequels = optLabelSequels;
         }
         // Set and sort the widths as number[]
         const widths = Object.getOwnPropertyNames(sequels)
@@ -71,14 +69,16 @@ const Pagy = {
             string.replace(/__pagy_page__/g, item)
                   .replace(/__pagy_label__/g, label);
 
-        pagyEl.pagyRender = () => {
+        pagyEl.pagyRender = function () {
             // Find the width that fits in parent
-            const width = widths.find((w) => pagyEl.parentElement !== null && pagyEl.parentElement.clientWidth > w) || 0;
+            const width = widths.find((w) =>
+                pagyEl.parentElement !== null && pagyEl.parentElement.clientWidth > w
+            ) || 0;
             // Only if the width changed
             if (width !== lastWidth) {
                 let html = tags.before;
                 const series = sequels[width.toString()];
-                const labels = label_sequels[width.toString()];
+                const labels = labelSequels[width.toString()];
                 for (const i in series) {
                     const item = series[i];
                     const label = labels[i];
@@ -100,20 +100,14 @@ const Pagy = {
             }
         };
         pagyEl.pagyRender();
-        // If there is a window object then add a single "resize" event listener
+        // If there is a window object then add a single throttled "resize" event listener
         if (typeof window !== "undefined") {
-            window.addEventListener("resize", Pagy.throttleRenderNavs, true);
+            let tid = 0;
+            window.addEventListener("resize", () => {
+                clearTimeout(tid);
+                tid = window.setTimeout(Pagy.renderNavs, 100);
+            }, true);
         }
-    },
-
-    // throttleRenderNavs properties
-    throttleRenderNavsTID:   0,      // private
-    throttleRenderNavsDelay: 100,    // set a default delay
-
-    // Avoid to fire the pagyRender multiple times
-    throttleRenderNavs() {
-        clearTimeout(Pagy.throttleRenderNavsTID);
-        Pagy.throttleRenderNavsTID = window.setTimeout(Pagy.renderNavs, Pagy.throttleRenderNavsDelay);
     },
 
     // Render all *nav_js helpers (i.e. all the elements of class "pagy-njs")
@@ -123,9 +117,9 @@ const Pagy = {
     },
 
     // Power the *_combo_nav_js helpers
-    combo_nav(pagyEl:Element, page:string, link:string, trimParam?:string) {
+    comboNav(pagyEl:Element, page:string, link:string, trimParam?:string) {
         const input = pagyEl.getElementsByTagName("input")[0];
-        const goToPage = () => {
+        Pagy.addInputBehavior(input, () => {
             if (page !== input.value) {
                 let html = link.replace(/__pagy_page__/, input.value);
                 if (typeof trimParam === "string" && input.value === "1") {
@@ -134,15 +128,14 @@ const Pagy = {
                 pagyEl.insertAdjacentHTML("afterbegin", html);
                 pagyEl.getElementsByTagName("a")[0].click();
             }
-        };
-        Pagy.addInputBehavior(input, goToPage);
+        });
     },
 
     // Power the pagy_items_selector_js helper
-    items_selector(pagyEl:Element, from:number, link:string, trimParam?:string) {
+    itemsSelector(pagyEl:Element, from:number, link:string, trimParam?:string) {
         const input = pagyEl.getElementsByTagName("input")[0];
         const current = input.value;
-        const goToPage = () => {
+        Pagy.addInputBehavior(input, () => {
             const items = input.value;
             if (items === "0" || items === "") {
                 return;
@@ -157,8 +150,7 @@ const Pagy = {
                 pagyEl.insertAdjacentHTML("afterbegin", html);
                 pagyEl.getElementsByTagName("a")[0].click();
             }
-        };
-        Pagy.addInputBehavior(input, goToPage);
+        });
     },
 
     // Add behavior to input fields
@@ -169,15 +161,10 @@ const Pagy = {
         input.addEventListener("focusout", goToPage);
         // goToPage when pressing enter while the input has focus
         input.addEventListener("keypress", (e:KeyboardEvent) => {
-            if (e.key === "Enter") {
-                goToPage();
-            }
+            if (e.key === "Enter") { goToPage() }
         });
     },
 
-    // Power the trim extra for js helpers
-    trim(link:string, param:string):string {
-        const re = new RegExp(`[?&]${param}=1\\b(?!&)|\\b${param}=1&`);
-        return link.replace(re, "");
-    }
+    trim: (link:string, param:string):string =>
+              link.replace(new RegExp(`[?&]${param}=1\\b(?!&)|\\b${param}=1&`), "")
 };
