80491ef1b3300b4df99e8994a25fd778baaf4f53
Domizio Demichelis <dd.nexus@gmail.com>
2022-01-13 10:18:55 +0700

    Version 5.7.4

--- src/pagy.ts
+++ src/pagy.ts
@@ -15,7 +15,7 @@ interface LabelSequels { readonly [width:string]: string[] }
 interface NavElement extends Element { pagyRender(): void }
 
 const Pagy = {
-    version: "5.7.3",
+    version: "5.7.4",
 
     // Scan for "data-pagy-json" elements, parse their JSON content and call their init functions
     init(arg?:Element|never) {
