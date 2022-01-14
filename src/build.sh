fc4f2b22343b7f82bccffa42827e37e49ccfb446
Domizio Demichelis <dd.nexus@gmail.com>
2022-01-14 17:26:23 +0700

    Updated build.sh

--- src/build.sh
+++ src/build.sh
@@ -3,17 +3,16 @@
 set -e
 
 echo 'generating pagy-dev.js...'
-tsc --inlineSources --inlineSourceMap
-mv pagy.js pagy-dev.js
+tsc --alwaysStrict --target esnext --inlineSources --inlineSourceMap pagy.ts
+mv -v pagy.js pagy-dev.js
 
 echo 'generating pagy-module.d.ts and pagy-module.js...'
-cp pagy.ts pagy-module.ts
-
+cp -v pagy.ts pagy-module.ts
 echo 'export default Pagy;' >> pagy-module.ts
-tsc --target esnext --declaration  pagy-module.ts
+tsc --alwaysStrict --target esnext --declaration pagy-module.ts
+rm -v pagy-module.ts
 
-echo 'generating pagy.js'
-parcel build
+echo 'generating pagy.js from main.js'
+parcel build   # nuke the distDir first
 
-mv -t ../lib/javascripts pagy-module.d.ts pagy-module.js pagy-dev.js
-rm pagy-module.ts
+mv -vt ../lib/javascripts pagy-module.d.ts pagy-module.js pagy-dev.js
