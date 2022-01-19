ce01b7a610b31b1b920c1ba5db96531e52b38497
BK <benkoshy@hotmail.com>
2022-01-19 17:10:01 +1100

    update: to test pagination with real data rows
    Please refer to [issue](https://github.com/ddnexus/pagy/issues/356)
    In order to reproduce it, some additional code was required - e.g.
    setting up connections, adding sqllite, with examples to allow for
    the easy reproduction of issues.
    
    Hopefully, with this PR, we will see the perfect reproduction of issues.
    
    I hope this helps.

--- apps/pagy_bare_rails.rb
+++ apps/pagy_bare_rails.rb
@@ -30,6 +30,7 @@ gemfile(true) do
   gem 'rails', '~> 6.1'
   gem 'actionpack'
   gem 'railties'
+  gem 'sqlite3'
 end
 
 # Set up debugging tools
@@ -43,6 +44,7 @@ end
 require 'pagy/extras/metadata'
 require 'pagy/extras/overflow'
 require 'pagy/extras/trim'
+require 'pagy/extras/gearbox'
 
 Pagy::DEFAULT[:trim_extra] = false
 Pagy::DEFAULT[:items] = 20
@@ -67,10 +69,18 @@ class TestApp < Rails::Application # :nodoc:
 
   routes.draw do
     get '/' => 'test#index', as: 'test'
+    get '/geared' => 'test#geared'
   end
 end
 
-class Record < ActiveRecord::Base; end # :nodoc:
+class Post < ActiveRecord::Base; end # :nodoc:
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+ActiveRecord::Base.connection.create_table(:posts)
+
+1.upto(11) do
+  Post.create
+end
 
 class TestController < ActionController::Base # :nodoc:
   include Rails.application.routes.url_helpers
@@ -78,8 +88,13 @@ class TestController < ActionController::Base # :nodoc:
   include Pagy::Backend
 
   def index
-    meta, _records = pagy(Record.none, items: 10)
-    render json: { data: [], meta: pagy_metadata(meta) }
+    meta, posts = pagy(Post.all, items: 10, gearbox_extra: false)
+    render json: { data: posts.as_json, meta: pagy_metadata(meta) }
+  end
+
+  def geared
+    _meta, posts = pagy(Post.all, gearbox_items: [3, 10])
+    render json: { data: posts }
   end
 
   ## override pagy methods if you need to here:
@@ -95,11 +110,17 @@ class TestControllerTest < ActionDispatch::IntegrationTest # :nodoc:
 
   test 'pagy json output - example' do
     get '/'
-    assert_equal response.parsed_body, { 'data' => [], 'meta' => { 'first_url' => '/?page=1', 'last_url' => '/?page=1' } }
+    assert_equal ({ 'data' => Post.first(10).as_json,
+                    'meta' => { 'first_url' => '/?page=1', 'last_url' => '/?page=2' } }), response.parsed_body
   end
 
   def test_my_pagy_problem_here
     assert false
+  end
+
+  def test_geared_page1
+    get '/geared'
+    assert_equal Post.first(3).as_json, response.parsed_body['data']
   end
 
   private
