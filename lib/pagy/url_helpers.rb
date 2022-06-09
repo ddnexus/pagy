8d3b33191d71bb20fffed50ee23a79a94790725a
Domizio Demichelis <dd.nexus@gmail.com>
2022-06-09 13:10:44 +0700

    Removed support for deprecated pagy_massage_params

--- lib/pagy/url_helpers.rb
+++ lib/pagy/url_helpers.rb
@@ -14,26 +14,10 @@ class Pagy
       params              = request.GET.merge(params)
       params[page_param]  = page
       params[items_param] = vars[:items] if vars[:items_extra]
-      query_string        = "?#{Rack::Utils.build_nested_query(pagy_deprecated_params(pagy, params))}"  # remove in 6.0
-      # params              = pagy.params.call(params) if pagy.params.is_a?(Proc)                       # add in 6.0
-      # query_string        = "?#{Rack::Utils.build_nested_query(params)}"                              # add in 6.0
+      params              = pagy.params.call(params) if pagy.params.is_a?(Proc)
+      query_string        = "?#{Rack::Utils.build_nested_query(params)}"
       query_string        = query_string.gsub('&', '&amp;') if html_escaped  # the only unescaped entity
       "#{request.base_url if absolute}#{request.path}#{query_string}#{vars[:fragment]}"
-    end
-
-    private
-
-    # Transitional code to handle params deprecations. It will be removed in version 6.0
-    def pagy_deprecated_params(pagy, params)    # remove in 6.0
-      if pagy.params.is_a?(Proc)                # new code
-        pagy.params.call(params)
-      elsif respond_to?(:pagy_massage_params)   # deprecated code
-        Warning.warn '[PAGY WARNING] The pagy_massage_params method has been deprecated and it will be ignored from version 6. ' \
-                     'Set the :params variable to a Proc with the same code as the pagy_massage_params method.'
-        pagy_massage_params(params)
-      else
-        params                                  # no massage params
-      end
     end
   end
 end
