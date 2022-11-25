7baaf87046f599fe7e36a151bc6f54d0b5be4d7a
Sunny Ripert <sunny@sunfox.org>
2022-11-25 13:30:07 +0100

    Remove aria-label="pager" on non-interactive attribute
    See also https://ericwbailey.website/published/aria-label-is-a-code-smell/

--- lib/pagy/frontend.rb
+++ lib/pagy/frontend.rb
@@ -21,7 +21,7 @@ class Pagy
       p_prev = pagy.prev
       p_next = pagy.next
 
-      html = +%(<nav#{p_id} class="pagy-nav pagination" aria-label="pager">)
+      html = +%(<nav#{p_id} class="pagy-nav pagination">)
       html << if p_prev
                 %(<span class="page prev">#{link.call p_prev, pagy_t('pagy.nav.prev'), 'aria-label="previous"'}</span> )
               else
