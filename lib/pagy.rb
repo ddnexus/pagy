3df94011f3b6c48ddd26fd7376a2652864ef2a8d
Domizio Demichelis <dd.nexus@gmail.com>
2022-12-21 13:56:39 +0700

    Version 6.0.0

--- lib/pagy.rb
+++ lib/pagy.rb
@@ -5,7 +5,7 @@ require 'pathname'
 
 # Core class
 class Pagy
-  VERSION = '5.10.1'
+  VERSION = '6.0.0'
 
   # Root pathname to get the path of Pagy files like templates or dictionaries
   def self.root
