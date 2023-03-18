5a0d73480c9434c18877e198d7722b998bb1c264
Domizio Demichelis <dd.nexus@gmail.com>
2023-03-18 08:59:13 +0700

    Admin: Updated pagy-ci workflow

--- test/coverage_setup.rb
+++ test/coverage_setup.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
 # Coverage by SimpleCov/CodeCov
 if ENV['CODECOV']
   require 'codecov' # require also simplecov
   # if you want the formatter to upload the results use SimpleCov::Formatter::Codecov instead
   SimpleCov.formatter = Codecov::SimpleCov::Formatter # upload with step in github actions
 elsif !ENV['CI']   # exclude in CI
-  require 'simplecov'
   SimpleCov.configure do
     command_name "Task##{$PROCESS_ID}"
     merge_timeout 20
@@ -24,5 +25,6 @@ elsif !ENV['CI']   # exclude in CI
   end
 
   SimpleCov.formatter = SimpleCov::Formatter::SimpleFormatter unless ENV.fetch('HTML_REPORTS', nil) == 'true'
-  SimpleCov.start
 end
+
+SimpleCov.start
