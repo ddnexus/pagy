14e7e514dd2f9566f0b78f448c151452e17e36fc
Filip Zachar <tulak45@gmail.com>
2023-05-04 10:15:25 +0200

    fix: Extras::Trim - fix trimming first page (#516)
    Co-authored-by: Domizio Demichelis <dd.nexus@gmail.com>

--- lib/pagy/extras/trim.rb
+++ lib/pagy/extras/trim.rb
@@ -14,7 +14,7 @@ class Pagy # :nodoc:
 
       lambda do |page, text = pagy.label_for(page), extra = ''|
         link = +link_proc.call(page, text, extra)
-        return link unless page == 1
+        return link unless page.to_s == '1'
 
         pagy_trim(pagy, link)
       end
