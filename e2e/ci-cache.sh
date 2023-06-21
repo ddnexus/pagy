36969b3ad9915c4f22e0706a261e31e633000f47
Domizio Demichelis <dd.nexus@gmail.com>
2023-06-21 18:09:31 +0700

    CI: Remove npm cache

--- e2e/ci-cache.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Create the cache file used in GitHub Actions to get the right e2e env
-set -e
-
-e2e_dir="$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")"
-cd "$e2e_dir"
-pnpm ls | tee "$e2e_dir/ci-cache.lock"
