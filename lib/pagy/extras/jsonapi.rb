2059569d0ab362d4729468a71928340206cc26cb
Domizio Demichelis <dd.nexus@gmail.com>
2024-01-18 11:45:54 +0700

    Better module overrides in jsonapi

--- lib/pagy/extras/jsonapi.rb
+++ lib/pagy/extras/jsonapi.rb
@@ -12,54 +12,71 @@ class Pagy # :nodoc:
     class ReservedParamError < StandardError
       # Inform about the actual value
       def initialize(value)
-        super("expected reserved :page param to be nil or Hash; got #{value.inspect}")
+        super("expected reserved :page param to be nil or Hash-like; got #{value.inspect}")
       end
     end
 
-    private
+    # Module overriding Backend
+    module BackendOverride
+      private
 
-    include UrlHelpers
+      include UrlHelpers
 
-    # Return the jsonapi links
-    def pagy_jsonapi_links(pagy, **opts)
-      { first: pagy_url_for(pagy, 1,         **opts),
-        last:  pagy_url_for(pagy, pagy.last, **opts),
-        prev:  pagy_url_for(pagy, pagy.prev, **opts),
-        next:  pagy_url_for(pagy, pagy.next, **opts) }
+      # Return the jsonapi links
+      def pagy_jsonapi_links(pagy, **opts)
+        { first: pagy_url_for(pagy, 1,         **opts),
+          last:  pagy_url_for(pagy, pagy.last, **opts),
+          prev:  pagy_url_for(pagy, pagy.prev, **opts),
+          next:  pagy_url_for(pagy, pagy.next, **opts) }
+      end
+
+      # Should skip the jsonapi
+      def pagy_skip_jsonapi?(vars)
+        return true if vars[:jsonapi] == false || (vars[:jsonapi].nil? && DEFAULT[:jsonapi] == false)
+        # check the reserved :page param
+        raise ReservedParamError, params[:page] unless params[:page].respond_to?(:fetch) || params[:page].nil?
+      end
+
+      # Override the Backend method
+      def pagy_get_page(vars)
+        return super if pagy_skip_jsonapi?(vars)
+        return 1 if params[:page].nil?
+
+        params[:page][vars[:page_param] || DEFAULT[:page_param]].to_i
+      end
     end
+    Backend.prepend BackendOverride
 
-    # Should skip the jsonapi
-    def pagy_skip_jsonapi?(vars)
-      return true if vars[:jsonapi] == false || (vars[:jsonapi].nil? && DEFAULT[:jsonapi] == false)
-      # check the reserved :page param
-      raise ReservedParamError, params[:page] unless params[:page].respond_to?(:fetch) || params[:page].nil?
+    # Module overriding ItemsExtra
+    module ItemsExtraOverride
+      private
+
+      # Override the ItemsExtra::Backend method
+      def pagy_get_items_size(vars)
+        return super if pagy_skip_jsonapi?(vars)
+        return if params[:page].nil?
+
+        params[:page][vars[:items_param] || DEFAULT[:items_param]]
+      end
     end
+    # Swap the next lines in ruby 3.0+ baseline
+    # ItemsExtra::Backend.prepend ItemsExtraOverride if defined?(ItemsExtra::Backend)
+    Backend.prepend ItemsExtraOverride if defined?(ItemsExtra::Backend)
 
-    # Override the Backend method
-    def pagy_get_page(vars)
-      return super if pagy_skip_jsonapi?(vars)
-      return 1 if params[:page].nil?
+    # Module overriding UrlHelper
+    module UrlHelperOverride
+      # Override UrlHelper method
+      def pagy_set_query_params(page, vars, params)
+        return super unless vars[:jsonapi]
 
-      params[:page][vars[:page_param] || DEFAULT[:page_param]].to_i
+        params['page'] ||= {}
+        params['page'][vars[:page_param].to_s]  = page
+        params['page'][vars[:items_param].to_s] = vars[:items] if vars[:items_extra]
+      end
     end
-
-    # Override the ItemsExtra::Backend method
-    def pagy_get_items_size(vars)
-      return super if pagy_skip_jsonapi?(vars)
-      return if params[:page].nil?
-
-      params[:page][vars[:items_param] || DEFAULT[:items_param]]
-    end
-
-    # Override UrlHelper method
-    def pagy_set_query_params(page, vars, params)
-      return super unless vars[:jsonapi]
-
-      params['page'] ||= {}
-      params['page'][vars[:page_param].to_s]  = page
-      params['page'][vars[:items_param].to_s] = vars[:items] if vars[:items_extra]
-    end
+    # Swap the next lines in ruby 3.0+ baseline
+    UrlHelpers.prepend UrlHelperOverride
+    # Backend.prepend UrlHelperOverride
+    # Frontend.prepend UrlHelperOverride
   end
-  Backend.prepend JsonApiExtra
-  Frontend.prepend JsonApiExtra
 end
