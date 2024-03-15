aeb215ac2e73e88280f11fd33565ac2ca3b3c012
Kobus Joubert <kobus51@yahoo.com>
2024-03-15 13:10:14 +0200

    Tests for jsonapi prev and next keys for unavailable links

--- test/pagy/extras/jsonapi_test.rb
+++ test/pagy/extras/jsonapi_test.rb
@@ -21,7 +21,7 @@ describe 'pagy/extras/jsonapi' do
     _ { _pagy, _records = app.send(:pagy, @collection) }.must_raise Pagy::JsonApiExtra::ReservedParamError
   end
 
-  describe "JsonApi" do
+  describe 'JsonApi' do
     it 'uses the :jsonapi with page:nil' do
       app = MockApp.new(params: { page: nil })
       pagy, _records = app.send(:pagy, @collection, items_extra: false)
@@ -37,46 +37,58 @@ describe 'pagy/extras/jsonapi' do
       _(app.send(:pagy_url_for, pagy, 2)).must_rematch :url_2
     end
   end
-  describe "Skip JsonApi" do
+  describe 'Skip JsonApi' do
     it 'skips the :jsonapi with page:nil' do
       Pagy::DEFAULT[:jsonapi] = false
       app = MockApp.new(params: { page: nil })
       pagy, _records = app.send(:pagy, @collection, items_extra: false)
-      _(app.send(:pagy_url_for, pagy, 1)).must_equal "/foo?page=1"
+      _(app.send(:pagy_url_for, pagy, 1)).must_equal '/foo?page=1'
       pagy, _records = app.send(:pagy, @collection)
-      _(app.send(:pagy_url_for, pagy, 1)).must_equal "/foo?page=1&items=20"
+      _(app.send(:pagy_url_for, pagy, 1)).must_equal '/foo?page=1&items=20'
       Pagy::DEFAULT[:jsonapi] = true
     end
     it 'skips the :jsonapi with page:3' do
       Pagy::DEFAULT[:jsonapi] = false
       app = MockApp.new(params: { page: 3 })
       pagy, _records = app.send(:pagy, @collection, items_extra: false)
-      _(app.send(:pagy_url_for, pagy, 2)).must_equal "/foo?page=2"
+      _(app.send(:pagy_url_for, pagy, 2)).must_equal '/foo?page=2'
       pagy, _records = app.send(:pagy, @collection)
-      _(app.send(:pagy_url_for, pagy, 2)).must_equal "/foo?page=2&items=20"
+      _(app.send(:pagy_url_for, pagy, 2)).must_equal '/foo?page=2&items=20'
       Pagy::DEFAULT[:jsonapi] = true
     end
   end
-  describe "JsonApi with custom named params" do
-    it "gets custom named params" do
+  describe 'JsonApi with custom named params' do
+    it 'gets custom named params' do
       app = MockApp.new(params: { page: { number: 3, size: 10 } })
       pagy, _records = app.send(:pagy, @collection, page_param: :number, items_param: :size)
       _(pagy.page).must_equal 3
       _(pagy.items).must_equal 10
     end
-    it "sets custom named params" do
+    it 'sets custom named params' do
       app = MockApp.new(params: { page: { number: 3, size: 10 } })
       pagy, _records = app.send(:pagy, @collection, page_param: :number, items_param: :size)
       _(app.send(:pagy_url_for, pagy, 4)).must_rematch :url
     end
   end
-  describe "#pagy_jsonapi_links" do
-    it "returns the ordered links" do
+  describe '#pagy_jsonapi_links' do
+    it 'returns the ordered links' do
       app = MockApp.new(params: { page: { number: 3, size: 10 } })
       pagy, _records = app.send(:pagy, @collection, page_param: :number, items_param: :size)
       result = app.send(:pagy_jsonapi_links, pagy)
       _(result.keys).must_equal %i[first last prev next] # not sure it's a requirementS
       _(result).must_rematch :result
+    end
+    it 'sets the prev value to null when the link is unavailable' do
+      app = MockApp.new(params: { page: { page: 1 } })
+      pagy, _records = app.send(:pagy, @collection)
+      result = app.send(:pagy_jsonapi_links, pagy)
+      _(result[:prev]).must_be_nil
+    end
+    it 'sets the next value to null when the link is unavailable' do
+      app = MockApp.new(params: { page: { page: 50 } })
+      pagy, _records = app.send(:pagy, @collection)
+      result = app.send(:pagy_jsonapi_links, pagy)
+      _(result[:next]).must_be_nil
     end
   end
 end
