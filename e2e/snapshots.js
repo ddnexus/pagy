8ed27ef86cb4bc138c8cd6949177ef4203801a51
Domizio Demichelis <dd.nexus@gmail.com>
2024-06-02 16:44:28 +0700

    Update npm modules

--- e2e/snapshots.js
+++ e2e/snapshots.js
@@ -313,7 +313,7 @@ module.exports = {
       "6": "<span id=\"pagy-info\"\n  class=\"pagy info\">Displaying items 981-1000 of 1000 in total</span>"
     }
   },
-  "__version": "13.8.1",
+  "__version": "13.10.0",
   "[repro] Test helpers": {
     "[repro] Test #nav": {
       "1": "<p id=\"records\">@records: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20</p>",
