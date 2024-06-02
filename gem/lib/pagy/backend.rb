ddnexus/pagy

commit 9a0bffadd1f932a9e085c8922392cd8fef0e6a90

Retrieve only @in items:
- improve the performance of the last page in
particular storage conditions (see #696)

gem/lib/pagy/backend.rb
@@ -38,7 +38,7 @@ class Pagy
     # Sub-method called only by #pagy: here for easy customization of record-extraction by overriding
     # You may need to override this method for collections without offset|limit
     def pagy_get_items(collection, pagy)
-      collection.offset(pagy.offset).limit(pagy.items)
+      collection.offset(pagy.offset).limit(pagy.in)
     end
   end
 end

gem/lib/pagy/extras/overflow.rb
@@ -27,7 +27,7 @@ class Pagy # :nodoc:
           initialize vars.merge!(page: @last)          # re-run with the last page
           @vars[:page] = requested_page                # restore the requested page
         when :empty_page
-          @offset = @items = @from = @to = 0           # vars relative to the actual page
+          @offset = @items = @in = @from = @to = 0     # vars relative to the actual page
           if defined?(Calendar) && is_a?(Calendar)     # only for Calendar instances
             edge = @order == :asc ? @final : @initial  # get the edge of the overflow side (neat, but any time would do)
             @from = @to = edge                         # set both to the edge utc time (a >=&&< query will get no records)

test/pagy/extras/overflow_test.rb
@@ -74,7 +74,7 @@ describe 'pagy/extras/overflow' do
       _(pagy.page).must_equal 100
       _(pagy.offset).must_equal 0
       _(pagy.items).must_equal 0
-      _(pagy.in).must_be_nil
+      _(pagy.in).must_equal 0
       _(pagy.from).must_equal 0
       _(pagy.to).must_equal 0
       _(pagy.prev).must_equal pagy.last

commit 8079b1eacd489751e0871dc14cd1c6203219fc7f

Fix: reversion caused by commit 9a0bffadd1f932a9e085c8922392cd8fef0e6a90
Why?

    * 704 - ms sql server seems to fail with the above commit.
    * We are not reverting the commit, but simply reverting back to the
`pagy_get_items` original implementation.

gem/lib/pagy/backend.rb
@@ -38,7 +38,7 @@ class Pagy
     # Sub-method called only by #pagy: here for easy customization of record-extraction by overriding
     # You may need to override this method for collections without offset|limit
     def pagy_get_items(collection, pagy)
-      collection.offset(pagy.offset).limit(pagy.in)
+      collection.offset(pagy.offset).limit(pagy.items)
     end
   end
 end