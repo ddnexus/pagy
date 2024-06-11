30735b13bb46700c30529c0a23548163b96f0a3d
Domizio Demichelis <dd.nexus@gmail.com>
2024-06-11 20:05:26 +0700

    Rename internal variables

--- scripts/update_top100.rb
+++ scripts/update_top100.rb
@@ -1,26 +1,27 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'http'
+require 'json'
 require 'net/http'
 
-URL_FMT   = 'https://api.github.com/repos/ddnexus/pagy/contributors?page=%s'
+USERS_URL_FMT   = 'https://api.github.com/repos/ddnexus/pagy/contributors?page=%s'
+COMMITS_URL_FMT = 'https://github.com/ddnexus/pagy/commits?author=%s'
 IMG_WIDTH = '40'
 MAX_COUNT = 100
 START_TAG = '<!-- top100 start -->'
 END_TAG   = '<!-- top100 end -->'
-count     = 0
-page      = 1
 
 top100 = "#{START_TAG}\n"
-until count >= MAX_COUNT || (contributors = JSON.parse(Net::HTTP.get(URI(format(URL_FMT, page))))).empty?
-  contributors.each do |c|
+count  = 0
+page   = 1
+until count >= MAX_COUNT || (users = JSON.parse(Net::HTTP.get(URI(format(USERS_URL_FMT, page))))).empty?
+  users&.each do |u|
     break if count >= MAX_COUNT
-    next if c['login'] == 'dependabot[bot]'
+    next if u['login'] == 'dependabot[bot]'
 
-    contribution = c['contributions'] == 1 ? 'contribution' : 'contributions'
-    top100 << %([<img src="#{c['avatar_url']}" width="#{IMG_WIDTH}" title="@#{
-    c['login']}: #{c['contributions']} #{contribution}">](https://github.com/ddnexus/pagy/commits?author=#{c['login']}))
+    contribution = u['contributions'] == 1 ? 'contribution' : 'contributions'
+    top100 << %([<img src="#{u['avatar_url']}" width="#{IMG_WIDTH}" title="@#{
+    u['login']}: #{u['contributions']} #{contribution}">](#{format(COMMITS_URL_FMT, u['login'])}))
     count += 1
   end
   page += 1
