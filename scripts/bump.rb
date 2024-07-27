830e90814ffb0faa0bef6b85615a6156271cbb94
Domizio Demichelis <dd.nexus@gmail.com>
2024-07-27 12:20:09 +0700

    Replace shell based bump script with bump.rb script

---
+++ scripts/bump.rb
@@ -0,0 +1,103 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'tempfile'
+require_relative 'scripty'
+
+# Abort if the working tree is dirty
+Scripty.die('Working tree dirty!') unless `git status --porcelain`.empty?
+
+# Prompt for the new version
+require_relative '../gem/lib/pagy'
+old_version = Pagy::VERSION
+puts "Current Pagy::VERSION: #{old_version}"
+print 'Enter the new version: '
+new_version = gets.chomp
+
+# Abort if the version is missing
+Scripty.die('Missing new version!') if new_version.empty?
+
+# Abort if the version is invalid
+new_fragments = new_version.split('.')
+Scripty.die('Incomplete semantic version!') if new_fragments.size < 3
+
+# Abort if there is no gem change
+Scripty.die("No gem changes since version #{old_version}!") \
+    if `git diff --name-only --relative=gem "#{old_version}"..HEAD`.empty?
+
+# Bump the version in files
+%w[retype.yml
+   .github/ISSUE_TEMPLATE/Code.yml
+   .github/latest_release_body.md
+   gem/apps/calendar.ru
+   gem/apps/demo.ru
+   gem/apps/rails.ru
+   gem/apps/repro.ru
+   gem/bin/pagy
+   gem/config/pagy.rb
+   gem/lib/pagy.rb
+   gem/pagy.gemspec
+   src/pagy.ts].each do |path|
+  Scripty.file_sub(path, old_version, new_version)
+end
+
+# Bumps docs example
+Scripty.file_sub('quick-start.md',
+                 old_version.split('.')[0, 2].join('.'),
+                 new_version.split('.')[0, 2].join('.'))
+
+# Build javascript files
+system(Scripty::ROOT.join('src/build').to_s)
+
+# Create a tempfile with the formatted changes from the gem-filtered gitlog
+gitlog  = Tempfile.new
+commits = `git rev-list "#{old_version}"..HEAD`.split("\n")
+commits.each do |commit|
+  next if `git show --pretty="format:" --name-only --relative=gem #{commit}`.empty?
+
+  subject = `git show --no-patch --format="- %s" #{commit}`.chomp
+  next if subject.match?('\[skip-log\]')
+
+  gitlog.puts subject
+  body = `git show --no-patch --format="%b" #{commit}`.chomp
+  next if body.empty?
+
+  lines = body.split("\n")
+  body  = lines.map { |line| "  #{line}" }.join("\n")
+  gitlog.puts body
+end
+gitlog.close
+
+# Edit the gitlog?
+Scripty.file_edit?('Gitlog', gitlog.path)
+
+# Prepare the .github/latest_release_body.md file
+# Used by .github/workflows/create_release.yml which is triggered by the :rubygem_release task (push tag)
+release_body_path = '.github/latest_release_body.md'
+# Insert whats_new from the README into latest_release_body file
+whats_new_content = Scripty.tagged_extract('README.md', 'whats_new')
+Scripty.tagged_file_sub(release_body_path, 'whats_new', whats_new_content)
+
+# Insert the changes into latest_release_body file
+changes = "\n#{File.read(gitlog.path)}"
+Scripty.tagged_file_sub(release_body_path, 'changes', changes)
+
+# Edit the Rlease Body?
+Scripty.file_edit?('Release Body', release_body_path)
+
+# Update the CHANGELOG
+Scripty.file_sub('CHANGELOG.md', /<hr>\n/, "<hr>\n\n## Version #{new_version}\n#{changes}")
+
+# Run the test to check the consistency of versioning across files
+system('bundle exec rake test_version')
+
+# Optional update of top 100
+Scripty.ask_and_do('Do you want to update the "Top 100 contributors"? (y/n)> ') do
+  require_relative 'update_top100'
+end
+
+# Optional commit
+Scripty.ask_and_do('Do you want to commit the changes? (y/n)> ') do
+  system('git add -A')
+  system("git commit -m 'Version #{new_version}'")
+end
---
+++ scripts/scripty.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'pathname'
+
+# A few functions useful in scripts
+module Scripty
+  ROOT = Pathname.new(`git rev-parse --show-toplevel`.chomp)
+
+  # Ask for confirmation and do
+  def ask_and_do(question)
+    print question
+    yield if gets.chomp.start_with?(/y/i)
+  end
+  module_function :ask_and_do
+
+  # Warn and exit
+  def die(message)
+    warn message
+    exit 1
+  end
+  module_function :die
+
+  # Optional edit
+  def file_edit?(name, filepath)
+    filepath = ROOT.join(filepath).to_s
+    puts "\n#{name}:"
+    puts File.read(filepath)
+    print "Do you want to edit #{name.inspect}? (y/n)> "
+    system("nano #{filepath}") if gets.chomp.start_with?(/y/i)
+  end
+  module_function :file_edit?
+
+  # Substitute a string in filepth
+  def file_sub(filepath, search, replace)
+    filepath = ROOT.join(filepath).to_s
+    content  = File.read(filepath)
+    content.sub!(search, replace)
+    File.write(filepath, content)
+  end
+  module_function :file_sub
+
+  # Substitute a tagged string in filepth
+  def tagged_file_sub(filepath, tag, new_content)
+    filepath = ROOT.join(filepath).to_s
+    content  = File.read(filepath)
+    content.sub!(/<!-- #{tag}_start -->.*<!-- #{tag}_end -->/m,
+                 "<!-- #{tag}_start -->#{new_content}<!-- #{tag}_end -->")
+    File.write(filepath, content)
+  end
+  module_function :tagged_file_sub
+
+  def tagged_extract(filepath, tag)
+    filepath = ROOT.join(filepath).to_s
+    content  = File.read(filepath)
+    content[/<!-- #{tag}_start -->(.*)<!-- #{tag}_end -->/m, 1]
+  end
+  module_function :tagged_extract
+end
