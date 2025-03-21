98e13a5f9bf7542e72f8666ea4e3d2912332a058
Marcus Deans <marcusddeans@outlook.com>
2025-03-21 13:42:30 -0500

    wip: add parsing and test functionality

--- gem/lib/pagy/extras/trim.rb
+++ gem/lib/pagy/extras/trim.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 class Pagy # :nodoc:
-  DEFAULT[:trim_extra] = true   # extra enabled by default
+  DEFAULT[:trim_extra] = true # extra enabled by default
 
   # Remove the page=1 param from the first page link
   module TrimExtra
@@ -22,7 +22,15 @@ class Pagy # :nodoc:
 
     # Remove the :page_param param from the first page anchor
     def pagy_trim(pagy, a)
-      a.sub!(/[?&]#{pagy.vars[:page_param]}=1\b(?!&)|\b#{pagy.vars[:page_param]}=1&/, '')
+      page_param = pagy.vars[:page_param]
+      pattern = if pagy.vars[:jsonapi]
+                  # Handle both URL-encoded (%5B, %5D) and non-encoded formats for robustness
+                  # Match both page[param]=1 and page%5Bparam%5D=1 formats
+                  /[?&]page(?:%5B|\[)#{page_param}(?:%5D|\])=1\b(?!&)|\bpage(?:%5B|\[)#{page_param}(?:%5D|\])=1&/
+                else
+                  /[?&]#{page_param}=1\b(?!&)|\b#{page_param}=1&/
+                end
+      a.sub!(pattern, '')
     end
   end
   Frontend.prepend TrimExtra
