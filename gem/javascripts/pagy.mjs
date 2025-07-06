cd0033cbbd0b6213e8661bb055ccd69f38e010f3
Domizio Demichelis <dd.nexus@gmail.com>
2025-07-06 08:52:33 +0000

    Version 9.3.5

--- gem/javascripts/pagy.mjs
+++ gem/javascripts/pagy.mjs
@@ -73,7 +73,7 @@ const Pagy = (() => {
   };
   const trim = (a, param) => a.replace(new RegExp(`[?&]${param}=1\\b(?!&)|\\b${param}=1&`), "");
   return {
-    version: "9.3.4",
+    version: "9.3.5",
     init(arg) {
       const target = arg instanceof Element ? arg : document;
       const elements = target.querySelectorAll("[data-pagy]");
--- src/pagy.ts
+++ src/pagy.ts
@@ -94,7 +94,7 @@ const Pagy = (() => {
 
   // Public interface
   return {
-    version: "9.3.4",
+    version: "9.3.5",
 
     // Scan for elements with a "data-pagy" attribute and call their init functions with the decoded args
     init(arg?:Element) {
