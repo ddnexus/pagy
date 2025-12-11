04ac24e8d9c69d36858ebe7b5038d7ad6f353d3e
Domizio Demichelis <dd.nexus@gmail.com>
2025-12-11 09:22:15 +0000

    💎 Add :current_url alias to :page_url

--- gem/lib/pagy/toolbox/helpers/page_url.rb
+++ gem/lib/pagy/toolbox/helpers/page_url.rb
@@ -4,12 +4,12 @@ class Pagy
   # Return the page url for any page
   def page_url(page, **)
     target = case page
-             when :first    then nil
-             when :current  then @page
-             when :previous then @previous
-             when :next     then @next
-             when :last     then @last
-             else                page
+             when :first          then nil
+             when :current, :page then @page
+             when :previous       then @previous
+             when :next           then @next
+             when :last           then @last
+             else                      page
              end
     compose_page_url(target, **) if target || page == :first
   end
