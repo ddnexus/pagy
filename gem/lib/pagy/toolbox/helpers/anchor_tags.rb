3a3d880acda00285fe6f0a5f539dbfb2704cc7a6
Domizio Demichelis <dd.nexus@gmail.com>
2026-01-16 11:45:14 +0000

    💎 Refactor anchor tag helpers

--- gem/lib/pagy/toolbox/helpers/anchor_tags.rb
+++ gem/lib/pagy/toolbox/helpers/anchor_tags.rb
@@ -4,22 +4,18 @@ require_relative 'support/a_lambda' # inheritable
 
 class Pagy
   # Return the enabled/disabled previous page anchor tag
-  def previous_tag(a = nil, text: I18n.translate('pagy.previous'),
-                   aria_label: I18n.translate('pagy.aria_label.previous'), **)
-    if @previous
-      (a || a_lambda(**)).(@previous, text, aria_label:)
-    else
-      %(<a role="link" aria-disabled="true" aria-label="#{aria_label}">#{text}</a>)
-    end
-  end
+  def previous_tag(...) = anchor_tag_for(:previous, ...)
 
   # Return the enabled/disabled next page anchor tag
-  def next_tag(a = nil, text: I18n.translate('pagy.next'),
-               aria_label: I18n.translate('pagy.aria_label.next'), **)
-    if @next
-      (a || a_lambda(**)).(@next, text, aria_label:)
-    else
-      %(<a role="link" aria-disabled="true" aria-label="#{aria_label}">#{text}</a>)
-    end
+  def next_tag(...) = anchor_tag_for(:next, ...)
+
+  private
+
+  def anchor_tag_for(which, a = nil, text: I18n.translate("pagy.#{which}"),
+                     aria_label: I18n.translate("pagy.aria_label.#{which}"), **)
+    page = send(which)
+    return (a || a_lambda(**)).(page.to_i, text, aria_label:) if page
+
+    %(<a role="link" aria-disabled="true" aria-label="#{aria_label}">#{text}</a>)
   end
 end
