988f406538dfbf02e6d35247f9f70e48adb262a7
Chris Oliver <excid3@gmail.com>
2026-02-04 18:54:52 -0600

    Fix NoMethodError with tampered params (#872)
    The page param could be an Array or Hash which will raise a NoMethodError for `to_i`. By casting to a String, this will fix the issue and return 0 when an invalid value is found.

--- gem/lib/pagy/classes/request.rb
+++ gem/lib/pagy/classes/request.rb
@@ -21,7 +21,7 @@ class Pagy
     def resolve_page(force_integer: true)
       page_key = @options[:page_key] || DEFAULT[:page_key]
       page     = @params.dig(@options[:root_key], page_key) || @params[page_key]
-      return [page.to_i, 1].max if force_integer
+      return [page.to_s.to_i, 1].max if force_integer
 
       page unless page == ''
     end
