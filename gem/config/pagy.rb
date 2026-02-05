d6f66d0e4463212c6565596011b625df0eee83ee
Retype GitHub Action <retype@object.net>
2026-02-05 01:39:17 +0000

    Refreshes Retype-generated documentation.
    Process triggered by ddnexus.

--- gem/config/pagy.rb
+++ gem/config/pagy.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Pagy initializer file (43.2.8)
+# Pagy initializer file (43.2.9)
 # See https://ddnexus.github.io/pagy/resources/initializer/
 
 ############ Global Options ################################################################
