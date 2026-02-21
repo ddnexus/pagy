d701e19b5b25df96cf1e533c2e35cbeb5c82fb4a
Retype GitHub Action <retype@object.net>
2026-02-21 03:31:44 +0000

    Refreshes Retype-generated documentation.
    Process triggered by ddnexus.

--- gem/config/pagy.rb
+++ gem/config/pagy.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Pagy initializer file (43.2.10)
+# Pagy initializer file (43.3.0)
 # See https://ddnexus.github.io/pagy/resources/initializer/
 
 ############ Global Options ################################################################
@@ -8,11 +8,12 @@
 # Add your global options below. They will be applied globally.
 # For example:
 #
-# Pagy.options[:limit] = 10               # Limit the items per page
-# Pagy.options[:client_max_limit] = 100   # The client can request a limit up to 100
-# Pagy.options[:max_pages] = 200          # Allow only 200 pages
-# Pagy.options[:jsonapi] = true           # Use JSON:API compliant URLs
+# Pagy::OPTIONS[:limit] = 10               # Limit the items per page
+# Pagy::OPTIONS[:client_max_limit] = 100   # The client can request a limit up to 100
+# Pagy::OPTIONS[:max_pages] = 200          # Allow only 200 pages
+# Pagy::OPTIONS[:jsonapi] = true           # Use JSON:API compliant URLs
 
+Pagy::OPTIONS.freeze
 
 ############ JavaScript ####################################################################
 # See https://ddnexus.github.io/pagy/resources/javascript/ for details.
--- gem/javascripts/pagy.mjs
+++ gem/javascripts/pagy.mjs
@@ -125,7 +125,7 @@ const Pagy = (() => {
     });
   };
   return {
-    version: "43.2.10",
+    version: "43.3.0",
     init(arg) {
       const target = arg instanceof HTMLElement ? arg : document, elements = target.querySelectorAll("[data-pagy]");
       for (const element of elements) {
