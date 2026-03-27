776241f9e61a26d0cb1907069e4902d3b088e27b
Retype GitHub Action <retype@object.net>
2026-03-27 02:13:36 +0000

    Refreshes Retype-generated documentation.
    Process triggered by ddnexus.

--- gem/config/pagy.rb
+++ gem/config/pagy.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Pagy initializer file (43.4.3)
+# Pagy initializer file (43.4.4)
 # See https://ddnexus.github.io/pagy/toolbox/configuration/initializer/
 
 
--- gem/javascripts/pagy.mjs
+++ gem/javascripts/pagy.mjs
@@ -128,7 +128,7 @@ export default (() => {
     });
   };
   return {
-    version: "43.4.3",
+    version: "43.4.4",
     init(arg) {
       const target = arg instanceof HTMLElement ? arg : document, elements = target.querySelectorAll("[data-pagy]");
       for (const element of elements) {
