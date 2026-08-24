6f0ebbe8cc8d77d6bfeaf40e001732b8a2fbb3b2
Domizio Demichelis <dd.nexus@gmail.com>
2026-08-24 16:54:45 +0700

    Version 43.6.2

--- gem/lib/pagy.rb
+++ gem/lib/pagy.rb
@@ -9,7 +9,7 @@ require_relative 'pagy/toolbox/helpers/loaders'
 
 # Top superclass
 class Pagy
-  VERSION     = '43.6.1'
+  VERSION     = '43.6.2'
   ROOT        = Pathname.new(__dir__).parent.freeze
   DEFAULT     = { limit: 20, limit_key: 'limit', page_key: 'page' }.freeze
   PAGE_TOKEN  = EscapedValue.new('P ')
